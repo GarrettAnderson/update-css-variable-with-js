9274362f2b025c2d5681811fd32ece4bf109580f
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-13 11:57:57 -0400

    created event listener for mouse over to trigger the handle update function. got the spacing the blur and the color to change

--- public/main.js
+++ public/main.js
@@ -4,6 +4,8 @@ function handleUpdate() {
   console.log(this.value)
   const suffix = this.dataset.sizing || ''
   console.log(suffix)
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
 inputs.forEach((input) => input.addEventListener('change', handleUpdate))
+inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate))
