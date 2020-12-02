5140df82b75aa5802d96478379288dab5188b837
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-12-02 14:47:13 -0500

    working on changing the value of the spacing input and added an on change function to change value on element

--- public/main.js
+++ public/main.js
@@ -1,11 +1,62 @@
-const inputs = document.querySelectorAll('.controls input')
+/* ========================
+    Garrett Custom JS
+   ======================== */
 
-function handleUpdate() {
-  console.log(this.value)
-  const suffix = this.dataset.sizing || ''
-  console.log(suffix)
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
-}
 
-inputs.forEach((input) => input.addEventListener('change', handleUpdate))
-inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate))
+let spacing = document.querySelector('#spacing')
+console.log(spacing)
+
+spacing.addEventListener('change', e => {
+  console.log('spacing clicked')
+  console.log(e.target.value)
+  // console.log(this)
+
+  spacing.style.padding(e.target.value)
+
+  console.log(spacing)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputs = document.querySelectorAll('.controls input')
+//
+// function handleUpdate() {
+//   console.log(this.value)
+//   const suffix = this.dataset.sizing || ''
+//   console.log(suffix)
+//   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+// }
+//
+// inputs.forEach((input) => input.addEventListener('change', handleUpdate))
+// inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate))
