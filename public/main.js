03d9fc02783693e32866cdfc6668a1f97a215965
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-12-02 16:07:11 -0500

    wrapped up the js for changing the css styles accordingly with event listeners. next i plan to refactor into less code because there is a bit of repetition in the code

--- public/main.js
+++ public/main.js
@@ -1,22 +1,54 @@
 /* ========================
-    Garrett Custom JS
-   ======================== */
+   Garrett Custom JS
+ ======================== */
 
 
-let spacing = document.querySelector('#spacing')
+let spacing = document.querySelector('input[name="spacing"]')
+let blur = document.querySelector('input[name="blur"]')
+let color = document.querySelector('input[name="base"]')
+let image = document.querySelector('img')
 console.log(spacing)
+console.log(blur)
+console.log(image)
+
 
 spacing.addEventListener('change', e => {
+// spacing.addEventListener('change', function(e) {
+
   console.log('spacing clicked')
   console.log(e.target.value)
-  // console.log(this)
-
-  spacing.style.padding(e.target.value)
-
+  // console.log(this) // refers to window object because of arrow function
   console.log(spacing)
 
-})
+  // change the value of padding on image
+  image.style.setProperty('padding', e.target.value + 'px')
+  console.log(image)
 
+/* =======================================================
+     QUESTION - why is the variable for css declaration not working in JS. Have to write padding explicitely
+
+
+     // image.style.setProperty('--base', e.target.value + 'px')
+
+  ======================================================= */
+});
+
+blur.addEventListener('change', e => {
+
+  console.log(blur)
+  console.log(e.target.value)
+  console.log(image)
+
+  image.style.setProperty('filter', 'blur' + '(' + e.target.value + 'px'  + ')')
+  console.log(image)
+
+});
+
+color.addEventListener('change', e => {
+
+  image.style.setProperty('background', e.target.value)
+
+})
 
 
 
